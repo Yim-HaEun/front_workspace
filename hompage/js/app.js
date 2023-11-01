48bdd573f20a7e466c411d75397c98102e7edba5
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2023-11-01 10:19:20 +0900

    ..

--- hompage/js/app.js
+++ hompage/js/app.js
@@ -1,7 +1,37 @@
 const images=[
-
+    './img/wtk (1).JPG',
+    './img/wtk (2).jpg',
+    './img/wtk (3).jpg',
+    './img/wtk (4).jpg',
+    './img/wtk (5).jpg',
+    './img/wtk (6).jpg',
+    './img/wtk (7).jpg',
+    './img/sp (1).JPG',
+    './img/sp (2).JPG',
+    './img/sp (3).JPG',
+    './img/sp (4).jpg',
+    './img/sp (5).jpg',
+    './img/sp (6).jpg',
+    './img/tk (1).jpg',
+    './img/tk (2).jpg',
+    './img/tk (3).jpg',
+    './img/tk (4).jpg',
+    './img/tk (5).JPG',
+    './img/tk (6).JPG',
+    './img/hw (1).JPG',
+    './img/hw (2).jpg',
+    './img/hw (3).jpg',
+    './img/hw (4).jpg',
+    './img/hw (5).jpg',
+    './img/hw (6).jpg',
+    './img/hk (1).jpg',
+    './img/hk (2).JPG',
+    './img/hk (3).jpg',
+    './img/hk (4).jpg',
+    './img/hk (5).jpg',
+    './img/hk (6).jpg',
 ];
-const imagesPerpage=6;//한 페이지에 표시될 이미지 수
+const imagesPerpage= 7;//한 페이지에 표시될 이미지 수
 let currentPage=0;//현재 페이지를 나타내는 변수, 초기값은 0으로 설정
 const imageContainer = document.querySelector('.img-container');
 const prevButton = document.getElementById('prevPage');//이전 버튼
@@ -9,8 +39,11 @@ const nextButton = document.getElementById('nextPage');//다음 버튼
 
 function displayImg(page){
     const startIndex = page*imagesPerpage;
+
     const endIndex = startIndex + imagesPerpage;
+    const pageImages = images.slice(startIndex,endIndex);
     imageContainer.innerHTML='';
+
     pageImages.forEach(image=>{
         const imgElement = document.createElement('img');
         imgElement.src = images;
@@ -18,11 +51,12 @@ function displayImg(page){
         imageContainer.appendChild(imgElement);
     });
 }
+
 //버튼 업데이트
 function updateButtons(){
     prevButton.disabled = currentPage===0;
     nextButton.disabled=(currentPage+1)*imagesPerpage>=images.length;
-}
+}//이전 버튼에대한 클릭이벤트
 prevButton.addEventListener('click',()=>{
     if(currentPage>0){
         currentPage--;
